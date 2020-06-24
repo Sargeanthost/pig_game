66f60b3a8f6d496684025ff90758d86599a0f780
Carlos Medina <sargeanthost@gmail.com>
2020-06-23 23:05:26 -0700

    Added source code v0.5

---
+++ pigGame.py
@@ -0,0 +1,146 @@
+import random as rd
+
+humanWins = False
+cpuWins = False
+isGameFinished = False
+humanHold = False
+cpuScore = 0
+cpuTempScore = 0
+humanScore = 0
+humanTempScore = 0
+turn = 0
+rolledA = 0
+
+#ask if you want to play
+
+ask = input('Would you like to play pigs y/n?  ')
+
+if ask.lower() == 'n' or ask.lower() == 'y' :
+    if ask.lower() == 'n':
+        quit() 
+else: 
+    print('Oops, learn what y/n means and try again!')
+    quit()    
+
+#setup
+
+def roll():
+    global rolledA
+    rolledA = rd.randint(1,6) 
+
+def is_above_100():
+    global cpuWins
+    global humanWins
+
+    if humanScore or cpuScore >= 100:
+        if turn % 2 == 1:
+            cpuWins = True
+        else:
+            humanWins = True
+    else:
+        humanWins = False
+        cpuWins = False
+
+
+while True and turn < 1000:
+    #turn one
+    
+    if turn == 0:
+
+        roll()
+        
+        #1 check
+        if rolledA == 1:
+            print('Looks like you rolled a 1. Better luck next time')
+            humanTempScore = 0
+            turn+=1
+            continue
+        
+        #immediately roll
+        print('You rolled a',rolledA, '!')
+        humanHold = int(input('Enter 1 to hold or 0 to continue rolling: '))
+        #continue rolling
+        while humanHold == 0:
+            roll()
+            #1 check
+            if rolledA ==1:
+                print('Looks like you rolled a 1. Better luck next time')
+                humanTempScore = 0
+                turn+=1
+                continue
+            print('You rolled a',rolledA,'!')
+            humanTempScore += rolledA
+            humanHold = int(input('Enter 1 to hold or 0 to continue rolling: '))
+        #held, store temp as perm score, advance to cpu
+        if humanHold == 1:
+            humanTempScore = humanScore
+            print('You held, smart')
+            turn += 1
+            continue
+         
+        # else:
+        #     turn += 1
+
+    #normal play
+
+    #cpu play
+    if turn % 1 == 0:
+        
+        roll()
+        #1check
+        if rolledA == 1:
+            print('Looks like I rolled a',rolledA,'. Better luck next time')
+            turn+=1
+        else:
+            print('I rolled a', rolledA,", Awesome!\nYour turn!")
+            cpuScore += rolledA
+            turn += 1
+
+    #human play     
+    if turn %2 == 0 :
+        roll()
+        
+        #1 check
+        if rolledA == 1:
+            print('Looks like you rolled a 1. Better luck next time')
+            humanTempScore = 0
+            turn+=1
+            continue
+        
+        #immediately roll
+        print('You rolled a',rolledA, '!')
+        humanHold = int(input('Enter 1 to hold or 0 to continue rolling: '))
+        #continue rolling
+        while humanHold == 0:
+            roll()
+            #1 check
+            if rolledA ==1:
+                print('Looks like you rolled a 1. Better luck next time')
+                humanTempScore = 0
+                turn+=1
+                continue
+            print('You rolled a',rolledA,'!')
+            humanTempScore += rolledA
+            humanHold = int(input('Enter 1 to hold or 0 to continue rolling: '))
+        #held, store temp as perm score, advance to cpu
+        if humanHold == 1:
+            humanTempScore = humanScore
+            print('You held, smart')
+            turn += 1
+            continue
+        # ###
+        # roll()
+        # print('You rolled a', rolledA ,'!')
+        # humanHold = int(input('Enter 1 to hold or 0 to continue rolling: '))
+        # if humanHold == 0:
+        #     while humanHold != 1:
+        #         print('You rolled a',roll(),'!')
+        #         humanHold 
+        #         humanHold = int(input('Enter 1 to hold or 0 to continue rolling: '))
+        # else:
+        #     turn += 1
+    else:
+        #debug line
+        print('A fatal error has occured on turn',turn,'. Check if conditionals resolve')
+        quit()
+print('escaped')
